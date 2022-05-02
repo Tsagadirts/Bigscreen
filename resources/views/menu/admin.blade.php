65abe72185e57df3e721d27eb7928e16a90e199a
mich <djackson2284@gmail.com>
2022-05-02 19:22:33 +0200

    layout
    Rajout du menu et layout admin

---
+++ resources/views/menu/admin.blade.php
@@ -0,0 +1,11 @@
+<div class="menu_admin">
+    <a href="{{ route("product.product.index") }}">
+        <span>Accueil</span>
+    </a> 
+    <a href="{{ route("dashboard") }}">
+        <span>Questions</span>
+    </a>
+    <a href="{{ route("sold") }}">
+        <span>Réponses</span>
+    </a>
+</div>
