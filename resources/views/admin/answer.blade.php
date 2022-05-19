c293095fa25da5377b925d3d7e41d3808633568e
mich <djackson2284@gmail.com>
2022-05-19 18:33:54 +0200

    commentaires

--- resources/views/admin/answer.blade.php
+++ resources/views/admin/answer.blade.php
@@ -3,7 +3,6 @@
 @section("content")
 
 <div class="questions">
-<<<<<<< HEAD
   <table class="table_questions table table-bordered">
     <thead class="thead text-center bg-secondary">
       <tr >
@@ -28,24 +27,6 @@
           <td colspan="3"></td>
         </tr>
       @endif
-=======
-  <table class="table_questions">
-    <thead class="thead-dark">
-      <tr>
-        <th >Numéro</th>
-        <th >Question</th>
-        <th >Type</th>
-      </tr>
-    @foreach ($answers as $answer)
-   </thead>
-   <tbody >
-     <tr>
-       {{-- -1 = 0 qui est le debut du tableau --}}
-       <td class="text-danger">{{ $questions[$answer->question_id-1]->id }}</</td>
-       <td >{{ $questions[$answer->question_id-1]->body }}</</td>
-       <td>{{ $answer->answer }}</</td>
-     </tr>
->>>>>>> 2fe89d7d0903968cbc82e48baef46abfdb2e66fd
     @endforeach
 </table>
 </div>
--- resources/views/users/sondage.blade.php
+++ resources/views/users/sondage.blade.php
@@ -5,39 +5,6 @@
     <div id="container">
       <div class="col-sm-10 col-sm offset-1">
         <h1 class="fw-bold">Bigscreen</h1>
-<<<<<<< HEAD
-         <p class="text-center">Merci de répondre a toutes les questions et de valider le formulaire en  bas de la page</p>
-         <form method="POST" action="/message">
-    @csrf
-    @foreach ($questions as $question)
-    <div>
-        <p class="text-justify">{{ $question->title }}</p>
-    </div>
-     @if ($question->question_type=='a')
-      <div>
-          <label for="{{ $question->id }}" placeholder="Default input">{{ $question->body }}</label>
-          <select name="{{ $question->id }}" id="{{ $question->id }}" class="form-select " aria-label="Floating label select example">
-              @foreach($question->options as $option)
-              <option value="{{$option}}">
-                {{$option}}
-              </option>
-              @endforeach
-          </select>
-        </div>    
-    @elseif($question->question_type=='b')
-      <div>
-          <label for="{{ $question->id }}" class="form-label">{{ $question->body }}</label>
-          <input type="text" name="{{ $question->id }}" id="{{ $question->id }}" class="form-control">
-      </div>    
-    @else
-    <div>
-      <p>{{ $question->body }}</p>
-      @for ($i = 1; $i<=5; $i++)
-        <div class="form-check form-check-inline">
-        <label for="{{ $question->id }}">{{$i}}</label>
-        <input type="radio" id="{{ $question->id }}"
-         name="{{ $question->id }}" value="{{$i}}">
-=======
         <p class="text-center">Merci de répondre a toutes les questions et de valider le formulaire en  bas de la page</p>
         <form method="POST" action="/message">
           @csrf
@@ -79,7 +46,6 @@
             <button type="submit" class="btn btn-light btn-lg btn-block">Finaliser</button>
           </div>
         </form>
->>>>>>> 2fe89d7d0903968cbc82e48baef46abfdb2e66fd
       </div>
     </div>
   
