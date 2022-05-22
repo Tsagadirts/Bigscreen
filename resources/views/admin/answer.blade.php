7fbab4a73d23a7243d5f9b9f9c3d67d2380c3b99
mich <djackson2284@gmail.com>
2022-05-22 23:49:16 +0200

    ..

--- resources/views/admin/answer.blade.php
+++ resources/views/admin/answer.blade.php
@@ -5,24 +5,24 @@
 <div class="questions">
   <table class="table_questions table table-bordered">
     <thead class="thead text-center">
-      <tr >
+      <tr>
         <th>Numéros</th>
         <th>Questions</th>
         <th>Réponses</th>
       </tr>
     </thead>
     {{-- la boucle affichera les questions et les réponses des utilisateurs pour la parti admin --}}
-      @foreach ($answers as $answer)
+      @foreach($answers as $answer)
       <tr>
         {{-- on recupère l'id de la questions --}}
-       <td >{{ $questions[$answer->question_id-1]->id }}</td>
+       <td>{{ $questions[$answer->question_id-1]->id }}</td>
         {{-- on recupère la questions --}}
        <td>{{ $questions[$answer->question_id-1]->body }}</td>
         {{-- on recupère les réponses de chaque questions --}}
        <td>{{ $answer->answer }}</td>
       </tr>
       {{-- if est un separateur après les vignt questions il crée un espace dans le tableau --}}
-      @if ($questions[$answer->question_id-1]->id % 20 == 0)
+      @if($questions[$answer->question_id-1]->id % 20 == 0)
         <tr class="separator">
           <td colspan="3"></td>
         </tr>
@@ -31,21 +31,6 @@
 </table>
 </div>
 @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
